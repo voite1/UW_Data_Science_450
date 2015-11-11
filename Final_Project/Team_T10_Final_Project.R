1e195ba6774396026b6e0639a5f26f71fdc98fd3
A.Kramer <voitel@gmail.com>
2015-11-10 20:54:53 -0800

    Worked on creating Weekend column, complete cases, removed duplicates

--- Final_Project/Team_T10_Final_Project.R
+++ Final_Project/Team_T10_Final_Project.R
@@ -1,14 +1,39 @@
 
 # Update the working directory for the place on your computer where you store the project
-setwd('/Users/voitel/TRAINING/UW_Data_Science/UW_Data_Science_450/Final_Project')
+# setwd('/Users/voitel/TRAINING/UW_Data_Science/UW_Data_Science_450/Final_Project')
+setwd('C:\\Users\\Aleksey\\Documents\\School\\UW_Data_Science\\UW_Data_Science_450\\Final_Project')
 
 # Designate file name for the download data
 file.name = "data.csv"
 
-# download file (For windows users method = "curl" may need to be removed)
-download.file("https://www.dropbox.com/s/xnsl4d6u3c3oper/train.csv?dl=0", destfile = file.name, method = "curl")
+# Variabled to hold data frame created from downloaded file named file.name
+data = ""
 
-# read CSV file in memory
-data = read.csv(file.name)
+# Usage of the local file named data.csv is preferred. If data is not available, usage of Mac is preferred.
+if(!file.exists(file.name)) {
+  # works on Mac only. R cannot download HTTPS links on windows any more
+  download.file("https://www.dropbox.com/s/xnsl4d6u3c3oper/train.csv?dl=0", destfile = file.name, method = "curl")
+} esle {
+  # read CSV file in memory
+  data = read.csv(file.name)
+}
 
-str(data)
+# Extract Complete Cases (if any)
+data = data[complete.cases(data),]
+
+# Remove duplicate rows (if any)
+# Not sure if this is really needed - we get a lot of data removed if we remove duplicates
+# But the question is 'Why there are so many duplicates?
+# Slow
+data = data[duplicated(data), ]
+
+# Add weekend column - works, takes couple of seconds
+data$Weekend = TRUE
+data[data$Weekday=="Saturday",]$Weekend = TRUE
+data[data$Weekday=="Sunday",]$Weekend = TRUE
+data[data$Weekday=="Monday",]$Weekend = FALSE
+data[data$Weekday=="Tuesday",]$Weekend = FALSE
+data[data$Weekday=="Wednesday",]$Weekend = FALSE
+data[data$Weekday=="Thursday",]$Weekend = FALSE
+data[data$Weekday=="Friday",]$Weekend = FALSE
+
